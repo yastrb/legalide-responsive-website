8d99157602528e1cd1d4f00d9226338b0de8ba3f
yastrb <uaalena202@gmail.com>
2023-01-18 16:13:56 +0200

    New Project

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Legalide Responsive Website
+
+
+
+## Responsive Website using HTML CSS & JavaScript 
+
+
+- Lightweight website made for law firm using HTML, CSS & JAVASCRIPT.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices and with a beautiful and pleasant user interface.
+
+
+
---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+﻿/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if (this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*==================== VIDEO ====================*/
+const videoFile = document.getElementById('video-file'),
+    videoButton = document.getElementById('video-button'),
+    videoIcon = document.getElementById('video-icon')
+
+function playPause() {
+    if (videoFile.paused) {
+        // Play video
+        videoFile.play()
+        // We change the icon
+        videoIcon.classList.add('ri-pause-line')
+        videoIcon.classList.remove('ri-play-line')
+    }
+    else {
+        // Pause video
+        videoFile.pause();
+        // We change the icon
+        videoIcon.classList.remove('ri-pause-line')
+        videoIcon.classList.add('ri-play-line')
+
+    }
+}
+videoButton.addEventListener('click', playPause)
+
+function finalVideo() {
+    // Video ends, icon change
+    videoIcon.classList.remove('ri-pause-line')
+    videoIcon.classList.add('ri-play-line')
+}
+// ended, when the video ends
+videoFile.addEventListener('ended', finalVideo)
+
+
+
+// ============ScrollReveal===========
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    // reset: true,
+})
+
+sr.reveal(`.home__data, .data, .sponsor__content`, {
+    origin: 'top',
+    interval: 100,
+})
+
+sr.reveal('.contact__item', {
+    origin: 'left',
+    interval: 200,
+})
